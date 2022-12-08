bfd71fcf0881971612f107c98a814125fb598ab2
lee <ikaros0569@gmail.com>
2022-12-08 11:40:46 +0900

    사용자 이름 입력 받는 기능 추가

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
-const test = 10;
+const headerUserNameElement = document.querySelector('.user');
+const userNameElement = document.querySelector('.user-name');
 
-console.log(test);
+const localUserName = localStorage.getItem('userName');
+
+if (localUserName) {
+  userNameElement.innerHTML = '${locatUserName} <span>님</span>';
+  headerUserNameElement.innerHTML = '${localUserName} <span>님</span>';
+}
+userNameElement.onclick = () => {
+  const userName = prompt('이름을 입력해 주세요');
+  localStorage.setItem('userName', userName);
+
+  headerUserNameElement.innerHTML = '${userName} <span>님</span>';
+  userNameElement.innerHTML = '${userName} <span>님</span>';
+};
+
