763c400d464a3360ca5bb3bd606df56e26934dcd
lee <ikaros0569@gmail.com>
2022-12-08 11:53:45 +0900

    클릭 영역 크게 잡히는 버그 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,24 @@
 const headerUserNameElement = document.querySelector('.user');
-const userNameElement = document.querySelector('.user-name');
+const userNameElement = document.querySelector('#name');
 
 const localUserName = localStorage.getItem('userName');
 
+
+const setUserNameInnerHtml = (name) => {
+  headerUserNameElement.innerHTML = '${name} <span>님</span>';
+  userNameElement.textContent = name;
+};
+
 if (localUserName) {
-  userNameElement.innerHTML = '${locatUserName} <span>님</span>';
-  headerUserNameElement.innerHTML = '${localUserName} <span>님</span>';
+  setUserNameInnerHtml(localUserName);
 }
+
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
+  // todo: userName에 대한 유효성 검사
+
   localStorage.setItem('userName', userName);
 
-  headerUserNameElement.innerHTML = '${userName} <span>님</span>';
-  userNameElement.innerHTML = '${userName} <span>님</span>';
+  setUserNameInnerHtml(userName);
 };
 
