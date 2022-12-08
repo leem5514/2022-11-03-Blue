b33a0d79290eabc66ae118fa73347f14bcf6690b
lee <ikaros0569@gmail.com>
2022-12-08 22:15:59 +0900

    사용자 이메일 학번 변경

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,6 @@ const userNameElement = document.querySelector('#name');
 
 const localUserName = localStorage.getItem('userName');
 
-
 const setUserNameInnerHtml = (name) => {
   headerUserNameElement.innerHTML = '${name} <span>님</span>';
   userNameElement.textContent = name;
@@ -15,10 +14,44 @@ if (localUserName) {
 
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
-  // todo: userName에 대한 유효성 검사
-
   localStorage.setItem('userName', userName);
 
   setUserNameInnerHtml(userName);
+};
+
+const userEmailElement = document.querySelector('#email');
+const localUserEmail = localStorage.getItem('userEmail');
+
+const userNumberElement = document.querySelector('#number');
+const localUserNumber = localStorage.getItem('userNumber');
+
+const setUserEmailInnerHtml = (email) => {
+  userEmailElement.textContent = email;
+};
+
+if (localUserEmail) {
+  setUserEmailInnerHtml(localUserEmail);
+}
+
+userEmailElement.onclick = () => {
+  const userEmail = prompt('이메일 주소를 입력해 주세요');
+  localStorage.setItem('userEmail', userEmail);
+
+  setUserEmailInnerHtml(userEmail);
+};
+
+const setUserNumberInnerHtml = (number) => {
+  userNumberElement.textContent = number;
+};
+
+if (localUserNumber) {
+  setUserNumberInnerHtml(localUserNumber);
+}
+
+userNumberElement.onclick = () => {
+  const userNumber = prompt('학번을 입력해 주세요');
+  localStorage.setItem('userNumber', userNumber);
+
+  setUserNumberInnerHtml(userNumber);
 };
 
